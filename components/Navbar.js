1ad866c7537134d64559db5d9dab2ba810d6d4c2
vietthom <thomle0418@gmail.com>
2022-11-07 16:45:21 -0700

    Basic layout of navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+
+
+export const Navbar = () => {
+  return (
+    <div className="text-white container mx-auto px-4 h-24 flex items-center">
+        <h1 className="w-full font-bold text-3xl">Stand Strong MMA</h1>
+        <ul className="flex items-center">
+            <li className="p-4">Home</li>
+            <li className="p-4">About</li>
+            <li className="p-4">Classes</li>
+            <li className="p-4">Membership</li>
+            <li className="p-4">Contact</li>
+        </ul>
+    </div>
+  )
+}
