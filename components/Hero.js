20613037b9e2602b306309f07ec6ae94dd117972
vietthom <thomle0418@gmail.com>
2022-11-08 16:21:48 -0700

    Add media queries to hero component.

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Hero = () => {
   return (
     <div className='max-w-[1240px] w-full h-screen mx-auto grid md:grid-cols-2 text-white'>
-        <div className='flex flex-col items-start mt-[96px]'>
+        <div className='xl:pl-0 lg:pl-6 md:pl-4 pl-6 flex flex-col items-start mt-[96px]'>
            <h1 className='md:text-6xl sm:text-5xl text-3xl font-bold text-red-400 py-4'>Welcome to Stand Strong MMA</h1>
            <p className='md:text-3xl sm:text-2xl text-xl font-bold py-4'>We believe in creating a safe, supportive, and inclusive space for all skill levels. We love training and teaching martial arts, and look forward to sharing our passion with you! </p>
            <h2 className='md:text-2xl text-xl font-bold text-gray-500 '>Interested in Training with us?</h2>
