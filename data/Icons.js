bcf18912b190d72ea426ca4e0352df7c56daa756
vietthom <thomle0418@gmail.com>
2022-11-09 21:46:09 -0700

    Data array for icon images

---
+++ data/Icons.js
@@ -0,0 +1,28 @@
+const Icons=[
+    {
+        name: 'MMA',
+        imageUrl: 'public/assets/icons/MMA-icon.png',
+    },
+    {
+        name: 'Kickboxing',
+        imageUrl: 'public/assets/icons/kickboxing-icon.png',
+    },
+    {
+        name: 'Muay Thai',
+        imageUrl: 'public/assets/icons/muaythai_icon.png',
+    },
+    {
+        name: 'Grappling',
+        imageUrl: 'public/assets/icons/grappling-icon.png',
+    },
+    {
+        name: 'Silat',
+        imageUrl: 'public/assets/icons/silat-icon.png',
+    },
+    {
+        name: 'Workout',
+        imageUrl: 'public/assets/icons/workout-icon.png',
+    },
+];
+
+export { Icons };
