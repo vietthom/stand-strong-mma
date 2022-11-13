4ee982e7d39bfff0f9205c189014367fda42a52e
vietthom <thomle0418@gmail.com>
2022-11-13 14:45:39 -0700

    Finished Footer component

---
+++ components/Footer.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { AiFillInstagram } from 'react-icons/ai';
+import { FaFacebook, FaTwitter, FaGithub } from 'react-icons/fa';
+
+const navigation={
+    main: [
+        {name: 'About', href: '#'}, 
+        {name: 'Classes', href: '#'},
+        {name: 'Membership', href: '#'},
+        {name: 'Contact', href: '#'},
+    ],
+    social: [
+        {
+            name: 'Facebook',
+            href: '#',
+            icon: ()=>(
+                <FaFacebook />
+            ),
+        },
+        {
+            name: 'Twitter', 
+            href: '#',
+            icon: ()=>(
+                <FaTwitter />
+            )
+        },
+        {
+            name: 'Github',
+            href: '#',
+            icon: ()=>(
+                <FaGithub />
+            )
+        },
+        {
+            name: 'Instagram',
+            href: '#', 
+            icon: ()=>(
+                <AiFillInstagram />
+            )
+        } 
+    ]
+};
+
+const Footer = () => {
+  return (
+    <div className='mx-auto max-w-[1240px] overflow-hidden py-12 px-4 sm:px-6 lg:px-8'>
+        <div className='-mx-5 -my-2 flex flew-wrap justify-center' aria-label="Footer">
+            {navigation.main.map((item) =>(
+                <div key={item.name} className='px-5 py-2'>
+                    <a href={item.href} className='text-base text-gray-500 hover:text-gray-900'>
+                        {item.name}
+                    </a>
+                </div>
+            ))}
+        </div>
+        <div className='mt-8 flex justify-center space-x-6'>
+            {navigation.social.map((item)=>(
+                <a key={item.name} href={item.href} className='text-gray-400 hover:text-gray-500'>
+                    <span className='sr-only'>{item.name}</span>
+                    <item.icon />
+                </a>
+            ))}
+        </div>
+        <p className='mt-8 text-center text-base text-gray-400'>2020 Stand Strong MMA, Inc. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Navbar  from "../components/Navbar"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
 import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Services />
       <Newsletter />
+      <Footer />
     </div>
   )
 }
